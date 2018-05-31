fea1fdda2a4539617a3cf7350bfe83d4feb4104f
harambeisnotdead <harambeisnotdead@users.noreply.github.com>
2018-05-31 19:53:24 +0200

    Updated endpoints

--- restapi/urls.py
+++ restapi/urls.py
@@ -4,24 +4,64 @@ from .views import *
 
 urlpatterns = [
     # Auth
-    path('get_auth_token/', rest_framework_views.obtain_auth_token, name='api_get_auth_token'),
+    path(
+        "get-auth-token",
+        rest_framework_views.obtain_auth_token,
+        name="api_get_auth_token",
+    ),
+
     # Citizens
-    path('citizen/create', CreateCitizenView.as_view(), name='citizen_create'),
-    path('citizen/<user__username>', SingleCitizenView.as_view(), name='citizen_record'),
+    path("citizen/create", CreateCitizenView.as_view(), name="citizen_create"),
+    path(
+        "citizen/<user__username>", SingleCitizenView.as_view(), name="citizen_record"
+    ),
+
     # Proposals
-    path('proposal/', ProposalView.as_view(), name='proposal_create_list'),
-    path('proposal/create', CreateProposalView.as_view(), name='proposal_create_list'),
-    path('proposal/<int:pk>', SingleProposalView.as_view(), name='proposal_record'),
-    path('proposal/most-debated/', MostDebatedProposalView.as_view(), name='most_debated_proposal'),
-    path('proposal/most-voted/', MostVotedProposalView.as_view(), name='most_voted_proposal'),
-    path('proposal/debating/', DebateProposalView.as_view(), name='debating_proposal'),
-    path('proposal/voting/', VoteProposalView.as_view(), name='voting_proposal'),
-    path('proposal/reviewing/', ReviewProposalView.as_view(), name='reviewing_proposal'),
+    path("proposals", ProposalView.as_view(), name="proposal_create_list"),
+    path("proposals/create", CreateProposalView.as_view(), name="proposal_create_list"),
+    path("proposals/<int:pk>", SingleProposalView.as_view(), name="proposal_record"),
+    path(
+        "proposals/most-debated",
+        MostDebatedProposalView.as_view(),
+        name="most_debated_proposal",
+    ),
+    path(
+        "proposals/most-voted",
+        MostVotedProposalView.as_view(),
+        name="most_voted_proposal",
+    ),
+    path("proposals/debating", DebateProposalView.as_view(), name="debating_proposal"),
+    path("proposals/voting", VoteProposalView.as_view(), name="voting_proposal"),
+    path(
+        "proposals/reviewing", ReviewProposalView.as_view(), name="reviewing_proposal"
+    ),
+
     # Votes CreateProposalVotingVoteView
-    path('proposal/reviewing/vote', ProposalReviewVoteView.as_view(), name='reviewing_vote_proposal'),
-    path('proposal/voting/vote', CreateProposalVotingVoteView.as_view(), name='voting_vote_proposal'),
+    path(
+        "proposals/reviewing/vote",
+        ProposalReviewVoteView.as_view(),
+        name="reviewing_vote_proposal",
+    ),
+    path(
+        "proposals/voting/vote",
+        CreateProposalVotingVoteView.as_view(),
+        name="voting_vote_proposal",
+    ),
+
     # Comments
-    path('proposal/<int:proposal>/comment/', CommentView.as_view(), name='proposal_comment'),
-    path('proposal/<int:proposal>/create-comment/', CreateCommentView.as_view(), name='proposal_comment'),
-    path('proposal/<int:proposal>/comment/nested/<int:comment>', CommentNestedView.as_view(), name='proposal_comment_nested'),
+    path(
+        "proposals/<int:proposal>/comments",
+        CommentView.as_view(),
+        name="proposal_comment",
+    ),
+    path(
+        "proposals/<int:proposal>/create-comment",
+        CreateCommentView.as_view(),
+        name="proposal_comment",
+    ),
+    path(
+        "proposals/<int:proposal>/nested-comment/<int:comment>",
+        CommentNestedView.as_view(),
+        name="proposal_comment_nested",
+    ),
 ]
